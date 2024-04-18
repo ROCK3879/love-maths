b773010d09d5adb5d29056593c4911bb87138fdd
Prela 9 <amir.prelic@gmail.com>
2024-04-18 10:08:34 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
+function runGame() {
 
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionalQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
